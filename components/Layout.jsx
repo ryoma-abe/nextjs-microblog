581d1643f0ebe54f65702096e3dfb0baeb70f472
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-19 21:04:51 +0900

    Layoutコンポーネントを追加し、index.jsで使用するように変更。画像のサイズを指定しました。

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,7 @@
-export const first = (second) => {
+import Head from "next/head";
+import Image from "next/image";
+
+export const Layout = () => {
   return (
     <>
       <Head>
@@ -6,7 +9,7 @@ export const first = (second) => {
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <header>
-        <Image src="/images/ryocode.png" />
+        <Image src="/images/ryocode.png" width={100} height={100} />
       </header>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import Link from "next/link";
-
+import { Layout } from "../components/Layout";
 export default function Home() {
   return (
     <>
@@ -11,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Link href="./posts/firstPost">firstPost</Link>
+      <Layout />
     </>
   );
 }
