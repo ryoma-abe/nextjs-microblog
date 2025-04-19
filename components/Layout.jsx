a222b77de333abd15570a14709d89acd3b5e1717
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-19 21:15:55 +0900

    Layout.jsxにサイトタイトルと名前を追加し、index.jsでLayoutコンポーネントをインポートして使用するように変更しました。

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
+const name = "RyoCode";
+export const siteTitle = "Next Blog";
 
 export const Layout = () => {
   return (
     <>
       <Head>
-        <title></title>
+        <title>{siteTitle}</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <header>
         <Image src="/images/ryocode.png" width={100} height={100} />
       </header>
+      <h1>{name}</h1>
     </>
   );
 };
