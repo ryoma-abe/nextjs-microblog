8016cccfedbc2c33585287f5193b0f835063d3af
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-20 21:18:05 +0900

    index.jsxにブログ記事のグリッド表示を追加し、Home.module.cssに新しいスタイルを適用しました。これにより、ブログセクションの視覚的な整頓が向上しました。

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Layout } from "../components/Layout";
 import utils from "../styles/utils.module.css";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +11,31 @@ export default function Home() {
       </section>
       <section>
         <h2>📝エンジニアのブログ</h2>
-        <div>
+        <div className={styles.grid}>
+          <article>
+            <Link href="#">
+              <img src="/images/thumbnail01.jpg" />
+            </Link>
+            <Link href="#">ブログタイトル1</Link>
+            <br />
+            <small>2024/11/16</small>
+          </article>
+          <article>
+            <Link href="#">
+              <img src="/images/thumbnail01.jpg" />
+            </Link>
+            <Link href="#">ブログタイトル1</Link>
+            <br />
+            <small>2024/11/16</small>
+          </article>
+          <article>
+            <Link href="#">
+              <img src="/images/thumbnail01.jpg" />
+            </Link>
+            <Link href="#">ブログタイトル1</Link>
+            <br />
+            <small>2024/11/16</small>
+          </article>
           <article>
             <Link href="#">
               <img src="/images/thumbnail01.jpg" />
