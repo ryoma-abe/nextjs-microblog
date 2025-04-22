7e770046a5338955c668a7f37669d88b28059161
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-23 06:57:52 +0900

    README.mdにNext.jsでの静的ブログ構築に関する基本知識を追加し、動的ルーティングのための`getStaticPaths`関数を`pages/posts/[id].js`に実装しました。これにより、全ての投稿IDを取得し、静的生成を強化しました。

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,14 @@
 import { Layout } from "@/components/Layout";
+import { getAllPostIds } from "@/lib/post";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default function post() {
-  return (
-    <Layout>
-      動的ルーティング
-    </Layout>
-  );
+  return <Layout>動的ルーティング</Layout>;
 }
