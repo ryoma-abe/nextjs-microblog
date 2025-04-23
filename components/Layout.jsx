5e5e353306c86326e8d8d827367b87887501b68d
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-24 07:09:54 +0900

    Layoutコンポーネントのヘッダー部分をリファクタリングし、`home`プロパティに基づく条件分岐を簡素化しました。これにより、コードの可読性が向上しました。

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,27 +13,17 @@ export const Layout = ({ children, home }) => {
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              className={`${utils.borderCircle} ${styles.headerHomeImage}`}
-              src="/images/profile.jpg"
-              width={100}
-              height={100}
-            />
-            <h1 className={utils.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Image
-              className={utils.borderCircle}
-              src="/images/profile.jpg"
-              width={100}
-              height={100}
-            />
-            <h1 className={utils.heading2Xl}>{name}</h1>
-          </>
-        )}
+        <Image
+          className={
+            home
+              ? `${utils.borderCircle} ${styles.headerHomeImage}`
+              : utils.borderCircle
+          }
+          src="/images/profile.jpg"
+          width={100}
+          height={100}
+        />
+        <h1 className={utils.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
